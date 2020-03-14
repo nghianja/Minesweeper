11a839bcb968a2e436c5c419d284d71627a16230
James Ng <james.ng@projapps.com>
2020-03-15 03:51:50 +0800

    Complete Stage #3

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -19,9 +19,42 @@ private fun plantMines(mines: Int): Array<CharArray> {
     return minefield
 }
 
+private fun calculateMines(minefield: Array<CharArray>) {
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            if (minefield[i][j] == '.') {
+                var count = 0
+                if (i - 1 >= 0) {
+                    if (j - 1 >= 0 && minefield[i - 1][j - 1] == 'X')
+                        count++
+                    if (minefield[i - 1][j] == 'X')
+                        count++
+                    if (j + 1 < rows && minefield[i - 1][j + 1] == 'X')
+                        count++
+                }
+                if (i + 1 < rows) {
+                    if (j - 1 >= 0 && minefield[i + 1][j - 1] == 'X')
+                        count++
+                    if (minefield[i + 1][j] == 'X')
+                        count++
+                    if (j + 1 < rows && minefield[i + 1][j + 1] == 'X')
+                        count++
+                }
+                if (j - 1 >= 0 && minefield[i][j - 1] == 'X')
+                    count++
+                if (j + 1 < rows && minefield[i][j + 1] == 'X')
+                    count++
+                if (count > 0)
+                    minefield[i][j] = "$count".first()
+            }
+        }
+    }
+}
+
 fun main() {
     print("How many mines do you want on the field? ")
     val minefield = plantMines(readLine()!!.toInt())
+    calculateMines(minefield)
     for (i in 1..rows) {
         for (j in 1..cols) {
             print(minefield[i - 1][j - 1])
--- Minesweeper/task/test/MinesweeperTest.java
+++ Minesweeper/task/test/MinesweeperTest.java
@@ -60,9 +60,9 @@ public class MinesweeperTest extends BaseStageTest<Integer> {
             }
 
             for (char c : line.toCharArray()) {
-                if (c != 'X' && c != '.') {
+                if (c != 'X' && c != '.' && !(c >= '0' && c <= '9')) {
                     return CheckResult.FALSE(
-                        "One of the characters is not equal to either 'X' or '.'.\n" +
+                        "One of the characters is not equal to 'X' or '.' or to a number.\n" +
                             "In this line: \"" + line + "\"."
                     );
                 }
@@ -76,6 +76,55 @@ public class MinesweeperTest extends BaseStageTest<Integer> {
             return CheckResult.FALSE(
                 "Expected to see " + attach + " mines, found " + mines
             );
+        }
+
+        int[] around = new int[] {-1, 0, 1};
+
+        for (int y = 0; y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < line.length(); x++) {
+                char c = line.charAt(x);
+
+                if (c == 'X') {
+                    continue;
+                }
+
+                int minesAround = 0;
+
+                for (int dx : around) {
+                    for (int dy : around) {
+
+                        int newX = x + dx;
+                        int newY = y + dy;
+
+                        if (0 <= newX && newX < 9 &&
+                            0 <= newY && newY < 9) {
+
+                            char newC = lines.get(newY).charAt(newX);
+
+                            if (newC == 'X') {
+                                minesAround++;
+                            }
+                        }
+                    }
+                }
+
+                if (minesAround == 0 && c != '.') {
+                    return CheckResult.FALSE(
+                        "There are no mines around, but found number " + c + ".\n" +
+                            "In line " + (y+1) + ", symbol " + (x+1) + "."
+                    );
+                }
+
+                if (minesAround != 0 && c != '0' + minesAround) {
+                    return CheckResult.FALSE(
+                        "In this cell should be number " + minesAround + ", " +
+                            "but found symbol \"" + c + "\".\n" +
+                            "In line " + (y+1) + ", symbol " + (x+1) + "."
+                    );
+                }
+
+            }
         }
 
         return CheckResult.TRUE;
