9887ef96b77366682f23ccc81ab3e7c1529b2e79
James Ng <james.ng@projapps.com>
2020-03-15 02:47:45 +0800

    Complete Stage #1

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,5 +1,25 @@
 package minesweeper
 
+import kotlin.random.Random
+
 fun main() {
-    print("Hello, World!")
+    val rows = 9
+    val cols = 9
+    val mines = 10
+    val minefield = Array<CharArray>(rows) { CharArray(cols) { '.' } }
+    var m = 0
+    while (m < mines) {
+        val i = Random.nextInt(1, rows) - 1
+        val j = Random.nextInt(1, cols) - 1
+        if (minefield[i][j] != 'X') {
+            minefield[i][j] = 'X'
+            m++
+        }
+    }
+    for (i in 1..rows) {
+        for (j in 1..cols) {
+            print(minefield[i - 1][j - 1])
+        }
+        println()
+    }
 }
