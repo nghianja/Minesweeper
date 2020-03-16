f46b02a43fdfabc85fed52d4db3e78f400bbba68
James Ng <james.ng@projapps.com>
2020-03-17 00:50:20 +0800

    Complete Stage #5

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -5,17 +5,44 @@ import kotlin.random.Random
 const val rows = 9
 const val cols = 9
 
-val minefield = Array(rows) { CharArray(cols) { '.' } }
-val mineboard = Array(rows) { CharArray(cols) { '.' } }
+var minefield = Array(rows) { CharArray(cols) { '.' } }
+var mineboard = Array(rows) { CharArray(cols) { '/' } }
+var dots = rows * cols
+var marked = 0
+var empty = 0
+var mines = 0
 
-private fun plantMines(mines: Int) {
-    var m = 0
-    while (m < mines) {
-        val i = Random.nextInt(1, rows) - 1
-        val j = Random.nextInt(1, cols) - 1
-        if (mineboard[i][j] != 'X') {
-            mineboard[i][j] = 'X'
-            m++
+private fun setup() {
+    minefield = Array(rows) { CharArray(cols) { '.' } }
+    dots = rows * cols
+    marked = 0
+    empty = 0
+    print("How many mines do you want on the field? ")
+    mines = readLine()!!.toInt()
+}
+
+private fun plantMines(y: Int, x: Int) {
+    if (mines <= rows * cols / 2) {
+        mineboard = Array(rows) { CharArray(cols) { '/' } }
+        var m = 0
+        while (m < mines) {
+            val i = Random.nextInt(1, rows) - 1
+            val j = Random.nextInt(1, cols) - 1
+            if (!(i == y && j == x) && mineboard[i][j] != 'X') {
+                mineboard[i][j] = 'X'
+                m++
+            }
+        }
+    } else {
+        mineboard = Array(rows) { CharArray(cols) { 'X' } }
+        var n = 0
+        while (n < rows * cols - mines) {
+            val i = Random.nextInt(1, rows) - 1
+            val j = Random.nextInt(1, cols) - 1
+            if ((i != y || j != x) && mineboard[i][j] != '/') {
+                mineboard[i][j] = '/'
+                n++
+            }
         }
     }
 }
@@ -23,14 +50,14 @@ private fun plantMines(mines: Int) {
 private fun calculateMines() {
     for (i in 0 until rows) {
         for (j in 0 until cols) {
-            if (mineboard[i][j] == '.') {
+            if (mineboard[i][j] == '/') {
                 var count = 0
                 if (i - 1 >= 0) {
                     if (j - 1 >= 0 && mineboard[i - 1][j - 1] == 'X')
                         count++
                     if (mineboard[i - 1][j] == 'X')
                         count++
-                    if (j + 1 < rows && mineboard[i - 1][j + 1] == 'X')
+                    if (j + 1 < cols && mineboard[i - 1][j + 1] == 'X')
                         count++
                 }
                 if (i + 1 < rows) {
@@ -38,21 +65,21 @@ private fun calculateMines() {
                         count++
                     if (mineboard[i + 1][j] == 'X')
                         count++
-                    if (j + 1 < rows && mineboard[i + 1][j + 1] == 'X')
+                    if (j + 1 < cols && mineboard[i + 1][j + 1] == 'X')
                         count++
                 }
                 if (j - 1 >= 0 && mineboard[i][j - 1] == 'X')
                     count++
-                if (j + 1 < rows && mineboard[i][j + 1] == 'X')
+                if (j + 1 < cols && mineboard[i][j + 1] == 'X')
                     count++
                 if (count > 0)
-                    minefield[i][j] = "$count".first()
+                    mineboard[i][j] = "$count".first()
             }
         }
     }
 }
 
-private fun printField() {
+private fun printField(exploded: Boolean = false) {
     println()
     print(" │")
     for (j in 1..cols)
@@ -65,7 +92,10 @@ private fun printField() {
     for (i in 1..rows) {
         print("$i│")
         for (j in 1..cols) {
-            print(minefield[i - 1][j - 1])
+            if (exploded && mineboard[i - 1][j - 1] == 'X')
+                print('X')
+            else
+                print(minefield[i - 1][j - 1])
         }
         println("│")
     }
@@ -75,40 +105,91 @@ private fun printField() {
     println("│")
 }
 
+private fun autoFree(x: Int, y: Int) {
+    if (minefield[y][x] == '.' || minefield[y][x] == '*') {
+        minefield[y][x] = mineboard[y][x]
+        dots--
+        if (minefield[y][x] == '/') {
+            if (y - 1 >= 0) {
+                if (x - 1 >= 0) autoFree(x - 1, y - 1)
+                autoFree(x, y - 1)
+                if (x + 1 < cols) autoFree(x + 1, y - 1)
+            }
+            if (y + 1 < rows) {
+                if (x - 1 >= 0) autoFree(x - 1, y + 1)
+                autoFree(x, y + 1)
+                if (x + 1 < cols) autoFree(x + 1, y + 1)
+            }
+            if (x - 1 >= 0) {
+                autoFree(x - 1, y)
+            }
+            if (x + 1 < cols) {
+                autoFree(x + 1, y)
+            }
+        }
+    }
+}
+
+private fun processMove(word: String, y: Int, x: Int): Boolean {
+    when (word) {
+        "mine" -> {
+            when (minefield[y][x]) {
+                '.' -> {
+                    minefield[y][x] = '*'
+                    if (mineboard[y][x] == 'X')
+                        marked++
+                    else
+                        empty++
+                }
+                '*' -> {
+                    minefield[y][x] = '.'
+                    if (mineboard[y][x] == 'X')
+                        marked--
+                    else
+                        empty--
+                }
+            }
+        }
+        "free" -> {
+            if (mineboard[y][x] == 'X') {
+                return true
+            }
+            autoFree(x, y)
+        }
+    }
+    return false
+}
+
 fun main() {
-    print("How many mines do you want on the field? ")
-    val mines = readLine()!!.toInt()
-    plantMines(mines)
-    calculateMines()
-    printField()
-    var marked = mines
-    var empty = 0
-    while (marked > 0 || empty > 0) {
+    setup()
+    do {
+        printField()
         print("Set/delete mines marks (x and y coordinates): ")
-        val (xstr, ystr) = readLine()!!.split(" ")
+        val (xstr, ystr, word) = readLine()!!.split(" ")
+        val x = xstr.toInt() - 1
+        val y = ystr.toInt() - 1
+        if (word == "free") {
+            plantMines(y, x)
+            calculateMines()
+        }
+        processMove(word, y, x)
+    } while (word != "free")
+    while (true) {
+        printField()
+        if ((marked == mines && empty == 0) || dots == mines) {
+            println("Congratulations! You found all mines!")
+            break
+        }
+        print("Set/delete mines marks (x and y coordinates): ")
+        val (xstr, ystr, word) = readLine()!!.split(" ")
         val x = xstr.toInt() - 1
         val y = ystr.toInt() - 1
         if (minefield[y][x].isDigit()) {
             println("There is a number here!")
-            continue
+        } else if (processMove(word, y, x)) {
+            printField(true)
+            println("You stepped on a mine and failed!")
+            break
         }
-        when (minefield[y][x]) {
-            '.' -> {
-                minefield[y][x] = '*'
-                if (mineboard[y][x] == 'X')
-                    marked--
-                else
-                    empty++
-            }
-            '*' -> {
-                minefield[y][x] = '.'
-                if (mineboard[y][x] == 'X')
-                    marked++
-                else
-                    empty--
-            }
-        }
-        printField()
     }
-    println("Congratulations! You found all mines!")
 }
